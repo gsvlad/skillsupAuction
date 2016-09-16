4fc652370b8924abed79380acb860a62c5dbdb07
gs_vlad <gs.vlad@gmail.com>
2016-09-16 23:16:59 +0300

    maven structure

--- auction-dao-api/src/main/java/com/su/auction/dao/api/GenericDao.java
+++
@@ -1,9 +0,0 @@
-package com.su.auction.dao.api;
-
-import java.util.List;
-
-public interface GenericDao<T> {
-    List<T> getAll();
-    void add(T entity);
-    void remove(T entity);
-}
--- auction-domain/src/main/java/com/su/domain/Item.java
+++
@@ -1,9 +0,0 @@
-package com.su.domain;
-
-import lombok.Data;
-
-@Data
-public class Item {
-    private String title;
-    private String description;
-}

9ba5802181329c092ae4027dc48f48968c2ba351
gs_vlad <gs.vlad@gmail.com>
2016-09-17 10:57:01 +0300

    clean project

--- auction-dao-api/src/main/java/com/su/auction/dao/api/ItemDao.java
+++
@@ -1,7 +0,0 @@
-package com.su.auction.dao.api;
-
-import com.su.domain.Item;
-
-public interface ItemDao extends GenericDao<Item> {
-
-}
--- auction-dao-api/src/main/java/com/su/auction/dao/api/UserDao.java
+++
@@ -1,7 +0,0 @@
-package com.su.auction.dao.api;
-
-import com.su.domain.User;
-
-public interface UserDao extends GenericDao<User> {
-
-}
